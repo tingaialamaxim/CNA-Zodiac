e382d2c5251e54194d563e146cba1d31a524e730
tingaialamaxim <tingaialamaxim@yahoo.com>
2021-04-20 13:27:09 +0300

    Added Zodiac Server

---
+++ Zodiac/ZodiacServer/ZodiacServer/DataAcces/ZodiacOperations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZodiacServer.DataAcces
+{
+    namespace ZodiacService.DataAccess
+    {
+        public class ZodiacOperations
+        {
+           
+            public static List<Tuple<Zodiac, Zodiac, string>> GetAllZodiacs(string filePath)
+            {
+                var zodiacs = new List<Tuple<Zodiac, Zodiac, string>>();
+
+                try
+                {
+                    var sr = new StreamReader(filePath);
+                    var line = sr.ReadLine()?.Split(" ");
+                    while (line != null)
+                    {
+                        zodiacs.Add(new Tuple<Zodiac, Zodiac, string>(
+                            new Zodiac { Date = line[0] },
+                            new Zodiac { Date = line[1] },
+                            line[2]));
+
+                        line = sr.ReadLine()?.Split(" ");
+                    }
+                    sr.Close();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("File Not Readable!");
+                }
+
+                return zodiacs;
+            }
+
+            public static string GetSign(string zodiacDate, string filePath)
+            {
+                var zodiacs = GetAllZodiacs(filePath);
+
+                return (from variable in zodiacs
+
+                        let startMonth = int.Parse(variable.Item1.Date.Substring(0, 2))
+                        let startDay = int.Parse(variable.Item1.Date.Substring(3, 2))
+                        let endMonth = int.Parse(variable.Item2.Date.Substring(0, 2))
+                        let endDay = int.Parse(variable.Item2.Date.Substring(3, 2))
+
+                        let date = zodiacDate.Split("/")
+                        let thisMonth = int.Parse(date[0])
+                        let thisDay = int.Parse(date[1])
+
+                        where thisMonth == startMonth && thisDay >= startDay || thisMonth == endMonth && thisDay <= endDay
+                        select variable.Item3).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ Zodiac/ZodiacServer/ZodiacServer/Services/ZodiacService.cs
@@ -0,0 +1,48 @@
+﻿using ZodiacServer.DataAcces.ZodiacService.DataAccess;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZodiacServer.DataAcces;
+
+
+namespace ZodiacServer.Services
+{
+    public class ZodiacService: Horoscope.HoroscopeBase
+    {
+        /*private readonly ZodiacOperations zodiacOperations = new ZodiacOperations();*/
+        public override Task<AddZodiacResponse> AddZodiac(AddZodiacRequest request, ServerCallContext context)
+        {
+            var zodiac = request.Zodiac;
+
+            if (zodiac.Date.Equals("Invalid Date"))
+            {
+                Console.WriteLine("\nDate Is Blank!");
+                return Task.FromResult(new AddZodiacResponse()
+                { Status = AddZodiacResponse.Types.Status.Error, Sign = "Invalid Sign" });
+            }
+
+          
+
+           /* var date = zodiac.Date.Split("/");
+            var thisYear = int.Parse(date[2]);
+            var thisMonth = int.Parse(date[0]);
+            var thisDay = int.Parse(date[1]);*/
+
+           /* var dateTime = new DateTime(thisYear, thisMonth, thisDay);*/
+            var sign = ZodiacOperations.GetSign(zodiac.Date,@".\Resources\zodiac.txt");
+           /* var client = new Horoscope.HoroscopeClient(channel);
+            var response = client.AddZodiac(new AddZodiacRequest { Zodiac });
+            */
+            
+        Console.WriteLine($"\nSign: {sign}\n");
+
+            return Task.FromResult(new AddZodiacResponse()
+            { Status = AddZodiacResponse.Types.Status.Success, Sign = sign });
+        }
+
+        
+    }
+}
